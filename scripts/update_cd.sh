b9b395847491e874d51f23c7b598122a89c8f434
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2024-02-29 11:23:31 -0500

    Rewrite `update_cd.sh` to work on both Linux and MacOS

--- scripts/update_cd.sh
+++ scripts/update_cd.sh
@@ -4,7 +4,12 @@
 # https://stackoverflow.com/questions/33225947/can-a-website-detect-when-you-are-using-selenium-with-chromedriver
 
 filename="${1:-/opt/chrome/chrome}"
+sed_cmd="sed -i"
+if [ "$(uname)" == "Darwin" ]; then
+    sed_cmd="sed -i ''"
+fi
 export LC_ALL=C
-sed -i '' -e 's/cdc_/dog_/g' "$filename"
-sed -i '' -e 's/wdc_/cat_/g' "$filename"
-sed -i '' -e 's/selenium/chocolat/g' "$filename"
+
+$sed_cmd -e 's/cdc_/dog_/g' "$filename"
+$sed_cmd -e 's/wdc_/cat_/g' "$filename"
+$sed_cmd -e 's/selenium/chocolat/g' "$filename"

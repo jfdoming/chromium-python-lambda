ad2f607b5c664a0cb744ede9851637b42f5128ab
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2024-03-10 20:51:06 -0400

    More flexibility in output format

--- local/pretty_output.py
+++ local/pretty_output.py
@@ -7,7 +7,11 @@ if "errorType" in resp:
     print(resp["errorMessage"], file=sys.stderr)
     sys.exit(1)
 elif "status" in resp:
-    print(resp["stdout"])
-    print(resp["stderr"], file=sys.stderr)
+    if "stdout" in resp:
+        print(resp["stdout"])
+    if "stderr" in resp:
+        print(resp["stderr"], file=sys.stderr)
     if resp["status"] == "error":
         sys.exit(1)
+    if "stdout" not in resp and "stderr" not in resp:
+        print(json.dumps(resp, indent=2))
